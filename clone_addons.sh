3d77c04ba6f2bb9327c07fa98ec417fc0b6a9c3e
Jeff Crouse <jefftimesten@gmail.com>
2015-06-24 11:01:55 +0100

    updated clone_addons

--- clone_addons.sh
+++ clone_addons.sh
@@ -2,3 +2,5 @@ git clone https://github.com/jefftimesten/ofxEdsdk.git ../../addons/ofxEdsdk
 git clone https://github.com/pach/ofxEtherdream.git ../../addons/ofxEtherdream
 git clone https://github.com/memo/ofxIlda.git ../../addons/ofxIlda
 git clone https://github.com/jefftimesten/ofxCanon.git ../../addons/ofxCanon
+git clone https://github.com/jefftimesten/ofxJSON.git ../../addons/ofxJSON
+git clone https://github.com/rezaali/ofxUI.git ../../addons/ofxUI
